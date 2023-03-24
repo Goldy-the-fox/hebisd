c023eed22484c732d9cf0ff5e9defca69f1aeaff
Goldy! <105525786+Goldy-the-fox@users.noreply.github.com>
2023-03-23 21:10:00 -0500

    Update themes.js

--- public/assets/js/settings/themes.js
+++ public/assets/js/settings/themes.js
@@ -1,17 +1,17 @@
 if (localStorage.getItem('Classic') === 'on') {
-  document.body.style.backgroundColor = "#111";
+  document.body.style.backgroundColor = "#015611";
   document.body.style.color = "#ffffff";
-  document.body.style.setProperty('--bg-c', "#111");
+  document.body.style.setProperty('--bg-c', "#015611");
   document.getElementById("Themes").value = "Classic";
 } else if (localStorage.getItem('Ocean') === 'on') {
   document.body.style.backgroundColor = "#162545";
   document.body.style.color = "#ffffff";
-  document.body.style.setProperty('--bg-c', "#162545");
+  document.body.style.setProperty('--bg-c', "#015611");
   document.getElementById("Themes").value = "Ocean";
 } else if (localStorage.getItem('Midnight') === 'on') {
-  document.body.style.backgroundColor = "#000";
+  document.body.style.backgroundColor = "#015611";
   document.body.style.color = "#ffffff";
-  document.body.style.setProperty('--bg-c', "#000");
+  document.body.style.setProperty('--bg-c', "#015611");
   document.getElementById("Themes").value = "Midnight";
 }
 
@@ -20,22 +20,22 @@ document.getElementById("Themes").onchange = function() {
     localStorage.setItem('Classic', 'on');
     localStorage.setItem('Ocean', 'off');
     localStorage.setItem('Midnight', 'off');
-    document.body.style.backgroundColor = "#111";
+    document.body.style.backgroundColor = "#015611";
     document.body.style.color = "#ffffff";
-    document.body.style.setProperty('--bg-c', "#111");
+    document.body.style.setProperty('--bg-c', "#015611");
   } else if (this.value === "Ocean") {
     localStorage.setItem('Ocean', 'on');
     localStorage.setItem('Classic', 'off');
     localStorage.setItem('Midnight', 'off');
-    document.body.style.backgroundColor = "#162545";
+    document.body.style.backgroundColor = "#015611";
     document.body.style.color = "#ffffff";
-    document.body.style.setProperty('--bg-c', "#162545");
+    document.body.style.setProperty('--bg-c', "#015611");
   } else if (this.value === "Midnight") {
     localStorage.setItem('Midnight', 'on');
     localStorage.setItem('Classic', 'off');
     localStorage.setItem('Ocean', 'off');
-    document.body.style.backgroundColor = "#000";
+    document.body.style.backgroundColor = "#015611";
     document.body.style.color = "#ffffff";
-    document.body.style.setProperty('--bg-c', "#000");
+    document.body.style.setProperty('--bg-c', "#015611");
   }
 };
